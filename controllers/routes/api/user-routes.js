d289fef75f538f4246c5a23f2ead3acd9ef74f6a
Rayman244 <rayhewitt42@icloud.com>
2022-02-08 00:34:11 -0500

    Mostly finished user routes

--- controllers/routes/api/user-routes.js
+++ controllers/routes/api/user-routes.js
@@ -1,40 +1,70 @@
-const router = require('express').Router()
-const {User} = require('../../../models')
+const router = require("express").Router();
+const { User } = require("../../../models");
 
-router.get('/all-users', (req, res) => {
-    // Using model in route
-    User.find({}, (err, result) => {
-      if (err) {
-        res.status(500).send({ message: 'Internal Server Error' });
-      } else {
-        res.status(200).json(result);
-      }
-    });
+// Get All Users
+router.get("/all-users", (req, res) => {
+  // Using model in route
+  User.find({}, (err, result) => {
+    if (err) {
+      res.status(500).send({ message: "Internal Server Error" });
+    } else {
+      res.status(200).json(result);
+    }
   });
-  router.get("/:id",(req,res)=>{
-      User.findById(req.params.id,(err,result)=>{
-          if(err){
-            res.status(500).json({ error: 'Something went wrong' });
-          }else{
-            res.status(200).json(result);
-          }
-      })
-  })
-  router.post("/", async(req,res)=>{
-      console.log();
-      try {
-      const {username,email} = await req.body
-      const newUser = await new User ({username:username,email:email})
-      if( !username || !email ){
-        res.status(500).json({ error: 'Please enter a valad email or password' });;
-      }else{
-        newUser.save()
-        res.status(201).json(newUser);
-      }
+});
 
+// Get by User Id
+router.get("/:id", (req, res) => {
+  User.findById(req.params.id, (err, result) => {
+    if (err) {
+      res.status(500).json({ error: "Something went wrong" });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+// Create a new User
+router.post("/", async (req, res) => {
+  console.log();
+  try {
+    const { username, email } = await req.body;
+    const newUser = await new User({ username: username, email: email });
+    if (!username || !email) {
+      res.status(500).json({ error: "Please enter a valad email or password" });
+    } else {
+      newUser.save();
+      res.status(201).json(newUser);
+    }
   } catch (error) {
-      console.log(error);
+    console.log(error);
   }
-      
+});
+
+// Update User
+router.put("/:id", async (req, res) => {
+  try{
+  const { username, email } = await req.body;
+ const updatedUser = await User.findByIdAndUpdate(req.params.id, { username: username, email: email });
+ if (!username || !email) {
+  res.status(500).json({ error: "Please enter a valad email or password" });
+} else {
+  updatedUser.save();
+  res.status(201).json(updatedUser);
+}
+} catch (error) {
+console.log(error);
+}
+});
+
+router.delete('/:id',(req,res)=>{
+  User.findByIdAndDelete(req.params.id,(err,result)=>{
+    if(result){
+      res.status(200).json(result)
+      console.log(`Successfully Deleted User`);
+    }else{
+      res.status(500).json({ error: `Something went wrong ${err}` });
+    }
   })
-module.exports = router
+})
+module.exports = router;
